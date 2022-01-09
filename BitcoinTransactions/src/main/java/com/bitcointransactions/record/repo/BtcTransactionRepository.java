6791cf8247d64fad5d2a8d87d92355451e3f4ca4
Chaitanya S Mujumdar <chaitanyamujumdar@gmail.com>
2022-01-09 17:15:11 +0530

    Delete BitcoinTransactions directory

--- BitcoinTransactions/src/main/java/com/bitcointransactions/record/repo/BtcTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bitcointransactions.record.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bitcointransactions.record.model.BtcTransaction;
-
-public interface BtcTransactionRepository extends JpaRepository<BtcTransaction,Integer> {
-
-}
